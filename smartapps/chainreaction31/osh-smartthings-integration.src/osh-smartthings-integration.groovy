82d0210be2a25fb8832db7b7dbb7329ca864e7cc
Ian Patterson <tipatterson31@gmail.com>
2017-08-08 15:11:58 -0500

    Push results to OSH with new format

--- smartapps/chainreaction31/osh-smartthings-integration.src/osh-smartthings-integration.groovy
+++ smartapps/chainreaction31/osh-smartthings-integration.src/osh-smartthings-integration.groovy
@@ -534,10 +534,9 @@ def insertResultTemplate(sensorName) {
 
 
 // schedule data polling (is executed once per minute)
-def scheduleHandler(){
-    def sensors = atomicState.URIs
-    def capMap = atomicState.sensorCaps
-    // TODO: examine responses to insertResultTemplate requests to isolate how to structure the posts in order to provide proper sensor data
+/*def scheduleHandler(){
+    def capMap = atomicState.URIs
+    //def capMap = atomicState.sensorCaps
     [motiondevices, humiditydevices, leakdevices, thermodevices, tempdevices, contactdevices,
      lockdevices, alarmdevices, switchdevices, presencedevices, smokedevices, buttondevices].each { n ->
         if (n != null){
@@ -547,9 +546,9 @@ def scheduleHandler(){
                 // create cs string to feed 
                 def dataString = ""
                 def count = 0
-                for ( capability in capMap.getAt(x.getLabel())){
+                for ( capability in capMap.getAt(removeSpaces(x.getLabel()))){
                     count++
-                    log.debug "Size of caps: " + capMap.getAt(x.getLabel()).size()
+                    log.debug "Size of caps: " + capMap.getAt(removeSpaces(x.getLabel())).size()
                     def size = capMap.getAt(x.getLabel()).size()
                     log.trace "[ln:604] Capability: " + capability
                     log.debug "Current Value of capability: " + x.currentValue(capability).toString()
@@ -593,7 +592,7 @@ def scheduleHandler(){
             }
     	} 
 	}
-}
+}*/
 
 // TODO: determine if there's a way to get the capability that triggered the even without resorting to versions for each type of capability (THERE IS)
 // TODO: could possibly have the capability doing the trigger passed in as a string (may be able to parse event.getData() or getName() for the info)
@@ -689,4 +688,56 @@ def addTimeRecord(){
     </swe:field>'''
 
     return timeRecord
+}
+
+// schedule data polling (is executed once per minute)
+def scheduleHandler(){
+    def capMap = atomicState.URIs
+    //def capMap = atomicState.sensorCaps
+    // TODO: examine responses to insertResultTemplate requests to isolate how to structure the posts in order to provide proper sensor data
+    [motiondevices, humiditydevices, leakdevices, thermodevices, tempdevices, contactdevices,
+     lockdevices, alarmdevices, switchdevices, presencedevices, smokedevices, buttondevices].each { n ->
+        if (n != null){
+
+            for (sensor in n){
+
+                for (capability in capMap.getAt(removeSpaces(sensor.getLabel()))) {
+                    log.debug "****Current Capability: " + capability + "****"
+
+                    //get current time
+                    log.debug "Current time: " + now()
+                    def currDate = new Date(now())
+                    log.debug "Current date: " + currDate
+                    String stringDate = currDate
+                    def time = getOSHDate(stringDate)
+                    log.debug time
+                    String dataString = sensor.currentValue(capability)
+
+                    try {
+                        def request = [
+                                //uri: 'http://146.148.39.135:8181/sensorhub/sos',
+                                uri               : endpoint,
+                                body              : '''<sos:InsertResult xmlns:sos="http://www.opengis.net/sos/2.0" service="SOS" version="2.0.0">
+    						        <sos:template>''' + capMap.getAt(removeSpaces(sensor.getLabel())) + '''</sos:template>
+   							        <sos:resultValues>''' + time + ',' + dataString + '''</sos:resultValues>
+    						        </sos:InsertResult>''',
+                                requestContentType: 'application/xml'
+                        ]
+
+                        log.debug "Insert Observation Request: " + request.body
+
+                        httpPost(request) { resp2 ->
+                            resp2.headers.each {
+                                //log.info "${it.name} : ${it.value}"
+                            }
+                            //log.debug "response contentType: ${resp2.contentType}"
+                            log.debug "response data: ${resp2.data}"
+                        }
+                    } catch (e) {
+                        log.error "Sending Data failed: $e"
+                    }
+                }
+            }
+        }
+    }
 }
